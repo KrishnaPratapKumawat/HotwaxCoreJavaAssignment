1392640ea401fb9d0650bfc2625713757e64719a
Krishna Pratap Kumawat <90032590+KrishnaPratapKumawat@users.noreply.github.com>
2022-01-02 21:18:49 +0530

    Add files via upload
    i have removed setter method in this file

--- src/SerializationDeserialization/AddressModel.java
+++ src/SerializationDeserialization/AddressModel.java
@@ -1,45 +1,49 @@
 package SerializationDeserialization;
 
 import java.io.*;
+import java.lang.*;
 
 public class AddressModel implements Serializable {
 
-    public String getCity() {
-        return city;
-    }
-    public void setCity(String city) {
-        this.city = city;
-    }
-    public String getState() {
-        return state;
-    }
-    public void setState(String state) {
-        this.state = state;
-    }
-    public Integer getPinCode() {
-        return pinCode;
-    }
-    public void setPinCode(Integer pinCode) {
-        this.pinCode = pinCode;
-    }
-    public String getCountry() {
-        return country;
-    }
-    public void setCountry(String country) {
-        this.country = country;
-    }
+        public String getCity ()
+        {
+            return city;
+        }
 
-    private String city;
-    private String state;
-    private Integer pinCode;
-    private String country;
+        public String getState ()
+
+        {
+            return state;
+        }
+
+        public Integer getPinCode ()
+        {
+            return pinCode;
+        }
+
+        public String getCountry ()
+        {
+            return country;
+        }
+        public void setCountry (String country)
+        {
+            this.country = country;
+        }
+
+        private String city;
+        private String state;
+        private Integer pinCode;
+        private String country;
 
     public AddressModel(String city, String state, Integer pinCode, String country) {
+
         this.city = city;
         this.state = state;
         this.pinCode = pinCode;
         this.country = country;
+
     }
+
 }
 
 
